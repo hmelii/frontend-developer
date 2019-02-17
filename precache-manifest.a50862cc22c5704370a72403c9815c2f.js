1fb0e7eba3082a255fc7f9bdae21efa909352d73
Andrey Stepanov <aanstepanov@rttv.ru>
2019-02-17 22:54:59 +0300

    Updates

---
+++ precache-manifest.a50862cc22c5704370a72403c9815c2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4db4aba572e109bee72",
+    "url": "/frontend-developer/static/js/runtime~main.d4db4aba.js"
+  },
+  {
+    "revision": "24be8fddd1a8bb8e2e3d",
+    "url": "/frontend-developer/static/js/main.24be8fdd.chunk.js"
+  },
+  {
+    "revision": "3e08b6564cc0b587f90f",
+    "url": "/frontend-developer/static/js/2.3e08b656.chunk.js"
+  },
+  {
+    "revision": "24be8fddd1a8bb8e2e3d",
+    "url": "/frontend-developer/static/css/main.3d94b22c.chunk.css"
+  },
+  {
+    "revision": "64961cd98cdfad5dfd31755002c4e5fe",
+    "url": "/frontend-developer/index.html"
+  }
+];
